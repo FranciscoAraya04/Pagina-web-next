1b7db64ff2716d9ceb8735002cf5bdab0a176386
FranciscoAraya04 <francisco.araya.work@gmail.com>
2024-10-04 22:03:16 -0300

    se agregó página contacto

---
+++ src/app/contact/page.js
@@ -0,0 +1,14 @@
+export default function ContactPage() {
+    return (
+      <div>
+        <h2>Contacto con el Dueño de Newton</h2>
+        <p>
+          Si tienes alguna pregunta o quieres recomendar un nuevo plato para que Newton pruebe,
+          no dudes en ponerte en contacto con su dueño, Francisco Araya.
+        </p>
+        <p>Correo electrónico: francisco.araya.work@gmail.com</p>
+        <p>Teléfono: +56984240423</p>
+      </div>
+    );
+  }
+  
